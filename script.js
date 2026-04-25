ca9c748bda722d329e87c9907f90e47066de6023
Azizi Keffli <Zeeykhm@gmail.com>
2026-04-26 01:22:23 +0800

    pwa

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", () => {
+		navigator.serviceWorker.register("/sw.js").catch(() => {
+			// Service worker registration can fail on local file preview; ignore quietly.
+		});
+	});
+}
 
